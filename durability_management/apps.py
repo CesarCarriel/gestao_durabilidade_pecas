8043a08ba136955a65e8c970a05b7f32dd537ccc
CesarCarriel <cesarcarrieldev@gmail.com>
2020-09-09 21:01:58 -0400

    feat: adding durabity_management

---
+++ durability_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DurabilityManagementConfig(AppConfig):
+    name = 'durability_management'
--- gestao_durabilidade_pecas/settings.py
+++ gestao_durabilidade_pecas/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'durability_management'
 ]
 
 ROOT_URLCONF = 'gestao_durabilidade_pecas.urls'
